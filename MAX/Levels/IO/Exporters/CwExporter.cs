da6da32459cbc056fb2b9947c463f1c13a7ba611
Random Strangers <110739593+RandomStrangers@users.noreply.github.com>
2025-05-12 15:00:21 -0400

    Update CwExporter.cs

--- MAX/Levels/IO/Exporters/CwExporter.cs
+++ MAX/Levels/IO/Exporters/CwExporter.cs
@@ -294,9 +294,9 @@ namespace MAX.Levels.IO {
 					list.ListType = (NbtTagType)reader.ReadByte();
 					var tags = new NbtTagType[ReadInt32()];
 					list.Tags.AddRange(tags);
-					for (int i = 0; i < list.Tags.COunt; i++) 
+					for (int i = 0; i < list.Tags.Count; i++) 
 					{
-						list.Tags[i] = (NbtTagType)ReadTag((byte)list.ListType, false).Value;
+						list.Tags[i] = (NbtTag)ReadTag((byte)list.ListType, false).Value;
 					}
 					tag.Value = list; 
 					break;
